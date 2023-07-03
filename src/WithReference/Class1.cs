321b253ded2eb402a06ef662329a8c03c4268e13
Mark <mark@rendlelabs.com>
2023-07-03 12:23:39 +0100

    Initial commit

---
+++ src/WithReference/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WithReference;
+public class Class1
+{
+    private readonly ILogger<Class1> _logger;
+
+    public Class1(ILogger<Class1> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Do(string thing)
+    {
+        _logger.LogInformation($"Doing {thing}...");
+    }
+}
